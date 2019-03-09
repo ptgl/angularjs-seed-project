9c59d963f2cafc429cf1fec5e4fc28133dc9ecb1
ptgl <pgialinh.iuetv@gmail.com>
2019-03-09 17:31:34 +0700

    add routing

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,19 @@
-var myApp = angular.module("myApp", []);
-myApp.controller("myCtrl", function($scope, $http){
+var myApp = angular.module("myApp", ["ngRoute"]);
+
+myApp.controller("myCtrl", function($scope, $http, $location){
     $scope.name = " Welcome to AngularJS"
+
+    $scope.goTo = function(url){
+      $location.path(url);
+    }
+})
+myApp.config(function($routeProvider) {
+    $routeProvider
+    .when("/", {
+      templateUrl : "view/login.html"
+    })
+    .when("/detail", {
+      templateUrl : "view/detail.html"
+    })
+    
 })
