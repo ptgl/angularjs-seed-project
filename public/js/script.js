7a62d57997492708a3dfaf5a08176dc0674f9641
ptgl <pgialinh.iuetv@gmail.com>
2019-03-10 11:35:03 +0700

    add login and detail page

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,39 @@
 var myApp = angular.module("myApp", ["ngRoute"]);
 
 myApp.controller("myCtrl", function($scope, $http, $location){
+    this.$onInit =  function(){
+      console.log('init data');
+      $scope.message = '';
+      fetch('/data.json').then(function(response){
+        return response.json();
+      }).then(function(data){
+        $scope.banks = data;
+        console.log(data);
+      })
+    }
+
     $scope.name = " Welcome to AngularJS"
 
-    $scope.goTo = function(url){
-      $location.path(url);
+    $scope.login = function(){
+      if($scope.userId == '14' && $scope.password == '9999')
+        $location.path('/detail');
+      else
+        $scope.message = "Try again please !"
+    }
+
+    $scope.view = function(){
+      console.log($scope.selected);
+      $scope.account = $scope.banks.bankList[$scope.selected];
     }
 })
 myApp.config(function($routeProvider) {
     $routeProvider
     .when("/", {
+      controller:'myCtrl',
       templateUrl : "view/login.html"
     })
     .when("/detail", {
+      controller:'myCtrl',
       templateUrl : "view/detail.html"
     })
     
